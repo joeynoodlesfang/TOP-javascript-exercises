cb84ffc01cf7b2b3cbca4f4e9a7e69e9da5fa7a6
jf <zhoufang29@gmail.com>
2024-10-02 01:39:57 -0700

    exercises 2,3,4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let myArr = string.split("");
+    let reversedArr = '';
+    for (let i = (myArr.length - 1); i >= 0; i--) {
+        
+        reversedArr += myArr[i];
+    }
+    return reversedArr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let ret = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!args.includes(array[i])) {
+            ret.push(array[i]);
+        }
+    }
+    return ret;
 };
 
 // Do not edit below this line
