c8cba563e88c12dbcaa8b97b1c0a97d7cecc1cbd
jf <zhoufang29@gmail.com>
2024-10-02 18:40:47 -0700

    exercises 5 6 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(begin, end) {
+    if (begin < 0 || end < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(begin) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+    let ret = 0;
+    if (begin < end) {
+        for (let i = begin; i <= end; i++) {
+            ret += i;
+        }
+    } else {
+        for (let i = begin; i >= end; i--) {
+            ret += i;
+        }
+    }
+    return ret;
 };
 
 // Do not edit below this line
